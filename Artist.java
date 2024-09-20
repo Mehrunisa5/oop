8a4dee4d67765ee714da2a6426d716b2ec39bb3b
Mehrunisa5 <mehrunisarafique31@gmail.com>
2024-09-20 16:44:50 -0700

    assignment

---
+++ Artist.java
@@ -0,0 +1,18 @@
+public class Artist{
+
+private String name;
+
+Artist( String name)
+{
+this.name = name;
+}
+public String getName()
+{
+return name;
+}
+public void display()
+{
+System.out.println("Name:" +name);
+}
+
+}
